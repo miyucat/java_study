1dbe3a53a401c900539455be90270732a5a05d93
miyucat <98681013+miyucat@users.noreply.github.com>
2022-02-12 20:46:24 +0800

    Add files via upload

---
+++ chapter01/Homework01.java
@@ -0,0 +1,7 @@
+public class Homework01
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello,work!");//輸出hello，word
+	}
+}
---
+++ chapter01/Homework02.java
@@ -0,0 +1,9 @@
+public class Homework02
+{
+	public static void main(String[] args)
+	{
+		//输出个人基本信息，用\t(制表键)分割,用\n换行
+		//设置文件编码  cmd（dos)需要gbk编码
+		System.out.println("姓名\t性别\t籍贯\t住址\n小猫\t保密\t保密\t保密");
+	}
+}
